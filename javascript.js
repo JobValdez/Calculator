9fc998a623d9331172bf433ae69b2119aa98012e
JobValdez <jodauran@gmail.com>
2022-05-26 09:27:56 -0400

    Letters are not acepted in the input was added

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 let result=document.getElementById("inputext");
 
+
 let calculate =(number)=>{
     result.value = result.value + number;
 }
-
 
 let Result=()=>{
     try {
@@ -11,7 +11,7 @@ let Result=()=>{
         result.value = eval(result.value);
 
     } catch (error) {
-        alert("Enter a valid input");
+        alert("Enter a valid digits");
     }
 }
 
@@ -21,4 +21,23 @@ function clr (){
 
 function del (){
     result.value = result.value.slice(0,-1);
+}
+
+const keyBoard = document.querySelector('.keyboard');
+
+keyBoard.addEventListener('keypress', e=>{
+
+    if (e.key === "/") {
+        console.log(e.key);
+        result.value = result.value + e.key + result.value.slice(-1,-1);
+    }
+
+})
+
+function onlyValidDigits(evt){
+
+    let characters = String.fromCharCode(evt.which);
+    if ( (/[a-zA-Z/]/g.test(characters))  ){
+        evt.preventDefault();   
+    }
 }
